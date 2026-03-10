b73526660ee66b6fd01c3baa0e2233c69f4b589c
mojjam07 <mojjam07@gmail.com>
2026-03-10 12:29:33 +0100

    fix admin and user navigation using react router Link_

--- src/router.jsx
+++ src/router.jsx
@@ -1,35 +1,35 @@
 // src/router/AppRouter.jsx
 import { Routes, Route } from "react-router-dom";
-import PrivateRoute from "../components/auth/PrivateRoute";
-import AuthLayout from "../components/layout/AuthLayout";
-import Layout from "../components/layout/Layout";
-import AdminLayout from "../components/layout/AdminLayout";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import AuthLayout from "./components/layout/AuthLayout";
+import Layout from "./components/layout/Layout";
+import AdminLayout from "./components/layout/AdminLayout";
 
 // Auth Pages
-import Login from "../pages/auth/Login";
-import Register from "../pages/auth/Register";
-import ForgotPassword from "../pages/auth/ForgotPassword";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
 
 // User Pages
-import Landing from "../pages/Landing";
-import UserDashboard from "../pages/user/UserDashboard";
-import Applications from "../pages/applications/Applications";
-import Documents from "../pages/documents/Documents";
-import Payment from "../pages/payment/Payment";
-import CostEstimation from "../pages/payment/CostEstimation";
-import Tracking from "../pages/tracking/Tracking";
-import Settings from "../pages/settings/Settings";
-import Biometrics from "../pages/biometrics/Biometrics";
-import Notifications from "../pages/notifications/Notifications";
+import Landing from "./pages/Landing";
+import UserDashboard from "./pages/user/UserDashboard";
+import Applications from "./pages/applications/Applications";
+import Documents from "./pages/documents/Documents";
+import Payment from "./pages/payment/Payment";
+import CostEstimation from "./pages/payment/CostEstimation";
+import Tracking from "./pages/tracking/Tracking";
+import Settings from "./pages/settings/Settings";
+import Biometrics from "./pages/biometrics/Biometrics";
+import Notifications from "./pages/notifications/Notifications";
 
 // Admin Pages
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import ApplicationReview from "../pages/admin/ApplicationReview";
-import UserManagement from "../pages/admin/UserManagement";
-import Reports from "../pages/admin/Reports";
-import AdminSettings from "../pages/admin/AdminSettings";
-import PassportManagement from "../pages/admin/PassportManagement";
-import DocumentManagement from "../pages/admin/DocumentManagement";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import ApplicationReview from "./pages/admin/ApplicationReview";
+import UserManagement from "./pages/admin/UserManagement";
+import Reports from "./pages/admin/Reports";
+import AdminSettings from "./pages/admin/AdminSettings";
+import PassportManagement from "./pages/admin/PassportManagement";
+import DocumentManagement from "./pages/admin/DocumentManagement";
 
 const AppRouter = () => (
   <Routes>
